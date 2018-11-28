05ad10580f1dc6eecc91854c53a612da71689658
John Clemis <dudejoe870@gmail.com>
2018-11-27 23:52:11 -0600

    Add the Lo0 / Lo1 differentiation

--- Ryu64.MIPS/Interpreter/R4300.cs
+++ Ryu64.MIPS/Interpreter/R4300.cs
@@ -127,7 +127,7 @@ namespace Ryu64.MIPS
             CycleCounter += Info.Cycles;
             Count        += Info.Cycles;
             Registers.COP0.Reg[Registers.COP0.COUNT_REG] = Count >> 1;
-            Registers.COP0.Reg[Registers.COP0.RANDOM_REG] -= 1;
+            --Registers.COP0.Reg[Registers.COP0.RANDOM_REG];
             if (Registers.COP0.Reg[Registers.COP0.RANDOM_REG] < Registers.COP0.Reg[Registers.COP0.WIRED_REG])
                 Registers.COP0.Reg[Registers.COP0.RANDOM_REG] = 0x1F; // TODO: Reset the Random Register to 0x1F after writing to the Wired Register.
 
--- Ryu64.MIPS/TLB.cs
+++ Ryu64.MIPS/TLB.cs
@@ -8,15 +8,19 @@ namespace Ryu64.MIPS
     {
         public struct TLBEntry
         {
-            public uint PFN;
-            public byte PageCoherency;
-            public byte Dirty;
-            public byte Valid;
-            public byte Global;
+            public uint PFN0;
+            public byte PageCoherency0;
+            public byte Dirty0;
+            public byte Valid0;
+            public byte Global0;
+            public uint PFN1;
+            public byte PageCoherency1;
+            public byte Dirty1;
+            public byte Valid1;
+            public byte Global1;
             public uint VPN2;
             public byte ASID;
             public ushort PageMask;
-            public bool hasBeenWrittenTo;
         }
 
         private readonly static TLBEntry[] TLBEntries = new TLBEntry[32];
@@ -25,17 +29,19 @@ namespace Ryu64.MIPS
         {
             foreach (TLBEntry Entry in TLBEntries)
             {
-                if (!Entry.hasBeenWrittenTo) continue;
-
                 uint VPN      = (uint)(((Entry.VPN2 << 13) | Entry.ASID) & ~((Entry.PageMask << 13) | 0x1FFF));
                 uint Mask     = (uint)((Entry.PageMask << 12) | 0x0FFF);
                 uint PageSize = Mask + 1;
 
                 if ((Address & (VPN | 0xE0000000)) != VPN) continue;
 
-                if (Entry.Valid == 0) throw new Common.Exceptions.TLBMissException("TLB Miss while translating address.");
+                uint Valid = ((Address & PageSize) != 1) ? Entry.Valid0 : Entry.Valid1;
 
-                return (Entry.PFN * PageSize) | (Address & Mask);
+                if (Valid == 0) continue;
+
+                uint PFN = ((Address & PageSize) != 1) ? Entry.PFN0 : Entry.PFN1;
+
+                return (PFN * PageSize) | (Address & Mask);
             }
 
             return Address;
@@ -54,13 +60,16 @@ namespace Ryu64.MIPS
         public static void ReadTLBEntry()
         {
             TLBEntry Entry = TLBEntries[(uint)Registers.COP0.Reg[Registers.COP0.INDEX_REG] & 0x3F];
-            uint EntryLo = (Entry.PFN << 6)
-                           | (byte)(Entry.Global & 0x1)
-                           | (byte)((Entry.Valid & 0x1) << 1)
-                           | (byte)((Entry.Dirty & 0x1) << 2)
-                           | (byte)((Entry.PageCoherency & 0b111) << 3);
-            Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] = EntryLo;
-            Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] = EntryLo;
+            Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] = (Entry.PFN0 << 6)
+                                                               | (byte)(Entry.Global0 & 0x1)
+                                                               | (byte)((Entry.Valid0 & 0x1) << 1)
+                                                               | (byte)((Entry.Dirty0 & 0x1) << 2)
+                                                               | (byte)((Entry.PageCoherency0 & 0b111) << 3);
+            Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] = (Entry.PFN1 << 6)
+                                                               | (byte)(Entry.Global1 & 0x1)
+                                                               | (byte)((Entry.Valid1 & 0x1) << 1)
+                                                               | (byte)((Entry.Dirty1 & 0x1) << 2)
+                                                               | (byte)((Entry.PageCoherency1 & 0b111) << 3);
 
             Registers.COP0.Reg[Registers.COP0.PAGEMASK_REG] = (uint)(Entry.PageMask << 13);
             Registers.COP0.Reg[Registers.COP0.ENTRYHI_REG] = (Entry.VPN2 << 13) | Entry.ASID;
@@ -70,27 +79,32 @@ namespace Ryu64.MIPS
         {
             TLBEntries[Index & 0x3F] = new TLBEntry()
             {
-                PFN           = (uint)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0x3FFFFFC0) >> 6),
-                Valid         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b000010)   >> 1),
-                Dirty         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b000100)   >> 2),
-                PageCoherency = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b111000)   >> 3),
+                PFN0           = (uint)((Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0x3FFFFFC0) >> 6),
+                Valid0         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0b000010)   >> 1),
+                Dirty0         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0b000100)   >> 2),
+                PageCoherency0 = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0b111000)   >> 3),
+                PFN1           = (uint)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0x3FFFFFC0) >> 6),
+                Valid1         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b000010) >> 1),
+                Dirty1         = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b000100) >> 2),
+                PageCoherency1 = (byte)((Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0b111000) >> 3),
                 VPN2          = (uint)((Registers.COP0.Reg[Registers.COP0.ENTRYHI_REG] & 0xFFFFE000)  >> 13),
                 PageMask      = (ushort)((Registers.COP0.Reg[Registers.COP0.PAGEMASK_REG] & 0x1FFF)   >> 13),
-                Global        = (byte)(((byte)Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0x1)
+                Global0       = (byte)(((byte)Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0x1)
                                      & ((byte)Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0x1)),
-                ASID = (byte)(Registers.COP0.Reg[Registers.COP0.ENTRYHI_REG] & 0xFF),
-                hasBeenWrittenTo = true
+                Global1       = (byte)(((byte)Registers.COP0.Reg[Registers.COP0.ENTRYLO0_REG] & 0x1)
+                                     & ((byte)Registers.COP0.Reg[Registers.COP0.ENTRYLO1_REG] & 0x1)),
+                ASID = (byte)(Registers.COP0.Reg[Registers.COP0.ENTRYHI_REG] & 0xFF)
             };
         }
 
         public static void ProbeTLB()
         {
-            bool hasFoundEntry = false;
+            bool FoundEntry = false;
             for (uint i = 0; i < TLBEntries.Length; ++i)
             {
                 TLBEntry Entry = TLBEntries[i];
 
-                if (Entry.Valid == 0) continue;
+                if ((Entry.Valid0 | Entry.Valid1) == 0) continue;
 
                 uint EntryHi = (uint)Registers.COP0.Reg[Registers.COP0.ENTRYHI_REG];
                 uint VPN2 = (EntryHi & 0xFFFFE000) >> 13;
@@ -98,13 +112,13 @@ namespace Ryu64.MIPS
 
                 if (Entry.VPN2 == VPN2 && Entry.ASID == ASID)
                 {
-                    hasFoundEntry = true;
+                    FoundEntry = true;
                     Registers.COP0.Reg[Registers.COP0.INDEX_REG] = i & 0x3F;
                     break;
                 }
             }
 
-            if (!hasFoundEntry) throw new Common.Exceptions.TLBMissException("Couldn't find TLB Entry while probing.");
+            if (!FoundEntry) throw new Common.Exceptions.TLBMissException("TLB Miss happened while probing the TLB.");
         }
     }
 }
