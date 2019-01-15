d988e8cfa44d5ee61dd8e3e03d10a1ee6167a262
John Clemis <dudejoe870@gmail.com>
2019-01-14 23:50:56 -0600

    Nothing to see here, except OH SHIT TRIANGLES ARE A THING NOW!!!!

--- Ryu64.RDP/RDP.cs
+++ Ryu64.RDP/RDP.cs
@@ -123,7 +123,7 @@ namespace Ryu64.RDP
                                     if (Common.Variables.Debug)
                                         Common.Logger.PrintInfoLine($"RDP | Fill Rectangle - XL: {FR_XL >> 2}.{FR_XL & 3}, YL: {FR_YL >> 2}.{FR_YL & 3}, XH: {FR_XH >> 2}.{FR_XH & 3}, YH: {FR_YH >> 2}.{FR_YH & 3}");
 
-                                    FillRect(FR_XL, FR_YL, FR_XH, FR_YH);
+                                    Rasterizer.FillRect(FR_XL, FR_YL, FR_XH, FR_YH);
                                     PC += 8;
                                     break;
                                 case 0x08: // Non-Shaded Triangle
@@ -147,6 +147,8 @@ namespace Ryu64.RDP
 
                                     if (Common.Variables.Debug)
                                         Common.Logger.PrintInfoLine($"RDP | Fill Triangle - YL: {NST_YL >> 2}, YM: {NST_YM >> 2}, YH: {NST_YH}, XL: {NST_XL >> 16}, DxLDy: {(short)(NST_DxLDy >> 16) + ((float)(NST_DxLDy & 0xFFFF) / 65535)}, XH: {NST_XH >> 16}, DxHDy: {(short)(NST_DxHDy >> 16) + ((float)(NST_DxHDy & 0xFFFF) / 65535)}, XM: {NST_XM >> 16}, DxMDy: {(short)(NST_DxMDy >> 16) + ((float)(NST_DxMDy & 0xFFFF) / 65535)}, Dir: {NST_Dir}");
+
+                                    Rasterizer.FillTriangle(NST_YL, NST_YM, NST_YH, NST_XL, NST_DxLDy, NST_XH, NST_DxHDy, NST_XM, NST_DxMDy, NST_Dir);
 
                                     PC += 32;
                                     break;
@@ -188,115 +190,6 @@ namespace Ryu64.RDP
             BPP8  = 1,
             BPP16 = 2,
             BPP32 = 3
-        }
-
-        private static void SetPixel(uint x, uint y, uint PixelSize, uint SX1, uint SY1, uint SX2, uint SY2, uint ImageWidth)
-        {
-            if ((x < SX1 || y < SY1) || (x > SX2 || y > SY2)) return;
-
-            uint Index = x + y * ImageWidth;
-            MIPS.R4300.memory.WriteUInt32(ColorImageAddr + (Index * PixelSize), FillColor);
-        }
-
-        public struct Vector2D
-        {
-            public uint X;
-            public uint Y;
-        }
-
-        public struct Triangle
-        {
-            public Vector2D A;
-            public Vector2D B;
-            public Vector2D C;
-        }
-
-        private static Triangle GetTriangle(ushort YL, ushort YM, ushort YH, uint XL, uint DxLDy, uint XH, uint DxHDy, uint XM, uint DxMDy, byte Dir)
-        {
-            Triangle Res = new Triangle();
-
-            ushort DxLDyRound = (ushort)(DxLDy >> 16);
-            ushort DxLDyFrac  = (ushort)(DxLDy & 0x0000FFFF);
-            ushort DxHDyRound = (ushort)(DxHDy >> 16);
-            ushort DxHDyFrac  = (ushort)(DxHDy & 0x0000FFFF);
-            ushort DxMDyRound = (ushort)(DxMDy >> 16);
-            ushort XLRound    = (ushort)(XL    >> 16);
-            ushort XHRound    = (ushort)(XH    >> 16);
-            ushort XMRound    = (ushort)(XM    >> 16);
-
-            ushort YLRound = (ushort)(YL >> 2);
-            ushort YMRound = (ushort)(YM >> 2);
-            ushort YHRound = (ushort)(YH >> 2);
-
-            float DxHDyFloat = (short)DxHDyRound + ((float)DxHDyFrac / 65535);
-            float DxLDyFloat = (short)DxLDyRound + ((float)DxLDyFrac / 65535);
-
-            Res.A = new Vector2D
-            {
-                X = (uint)((DxHDyFloat * (uint)(YLRound - YHRound)) + XMRound),
-                Y = YLRound
-            };
-
-            Res.B = new Vector2D
-            {
-                X = XLRound,
-                Y = YMRound
-            };
-
-            Res.C = new Vector2D
-            {
-                X = XMRound,
-                Y = YHRound
-            };
-
-            return Res;
-        }
-
-        private static void FillRect(ushort XL, ushort YL, ushort XH, ushort YH)
-        {
-            uint ImageWidth = ColorImageWidth + (uint)1;
-
-            // Get the X and Y for the points of the rectangle by shifting the 10.2 format X and Y parameters by 2 to the right.
-            uint RectX1 = (uint)(XH >> 2);
-            uint RectY1 = (uint)(YH >> 2);
-            uint RectX2 = (uint)(XL >> 2) + 1;
-            uint RectY2 = (uint)(YL >> 2) + 1;
-
-            // Pre Calculate the Screen Borders using the 10.2 format Scissor variables.
-            uint ScreenX1 = (uint)(ScissorXH >> 2);
-            uint ScreenY1 = (uint)(ScissorYH >> 2);
-            uint ScreenX2 = (uint)(ScissorXL >> 2);
-            uint ScreenY2 = (uint)(ScissorYL >> 2);
-
-            // Pre Calculate the final point positions.
-            uint x1 = RectX1;
-            uint y1 = RectY1;
-            uint x2 = RectX2;
-            uint y2 = RectY2;
-
-            if (ColorImageFormat == (uint)ImageFormat.RGBA)
-            {
-                if (ColorImageSize == (uint)BPP.BPP32)
-                {
-                    for (uint y = y1; y < y2; ++y)
-                        for (uint x = x1; x < x2; ++x)
-                            SetPixel(x, y, 4, ScreenX1, ScreenY1, ScreenX2, ScreenY2, ImageWidth);
-                }
-                else if (ColorImageSize == (uint)BPP.BPP16)
-                {
-                    for (uint y = y1; y < y2; ++y)
-                        for (uint x = x1; x < x2; x += 2)
-                            SetPixel(x, y, 2, ScreenX1, ScreenY1, ScreenX2, ScreenY2, ImageWidth);
-                }
-                else
-                {
-                    Common.Logger.PrintWarningLine($"RDP | {(BPP)ColorImageSize} isn't supported by RGBA");
-                }
-            }
-            else
-            {
-                throw new NotImplementedException($"Rectangles aren't supported in any other mode other than RGBA currently.");
-            }
         }
     }
 }
